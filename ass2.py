daa63cced80272a6fd2572faef84e0a7c93cea51
Yogesh Mandawat <yogeshmandawat2324@gmail.com>
2025-12-16 18:51:13 +0530

    I forget to uncommit the code...

--- ass2.py
+++ ass2.py
@@ -1,114 +1,115 @@
-# # problem number 02 
+# problem number 02 
 
 
-# def find_even(a, b):
-#     evn_num = []
-#     add_num = []
+def find_even(a, b):
+    evn_num = []
+    add_num = []
     
-#     for i in range(a, b + 1):
-#         if i % 2 == 0:
-#             evn_num.append(i)
+    for i in range(a, b + 1):
+        if i % 2 == 0:
+            evn_num.append(i)
         
-#         elif i % 2 != 0:
-#             add_num.append(i)
+        elif i % 2 != 0:
+            add_num.append(i)
     
-#     print("even numbers is : ", evn_num)
-#     print("odd numbers is : ", add_num)
+    print("even numbers is : ", evn_num)
+    print("odd numbers is : ", add_num)
     
-#     return evn_num, add_num
+    return evn_num, add_num
 
-# a = int(input("enter the first number : "))
-# b = int(input("enter the second number : "))
-# find_even(a, b)
+a = int(input("enter the first number : "))
+b = int(input("enter the second number : "))
+find_even(a, b)
 
 
 # problem number 03 - print digits of a number seperately
 
    
-# def num_split(n):
+def num_split(n):
     
-#     num_list = []
-#     for i in range(n):
-#         x1 = n % 10
-#         num_list.append(x1)
-#         n = n // 10
-#         if n == 0:
-#             break
+    num_list = []
+    for i in range(n):
+        x1 = n % 10
+        num_list.append(x1)
+        n = n // 10
+        if n == 0:
+            break
         
-#     print(num_list[::-1])
+    print(num_list[::-1])
     
-# n = int(input("enter the number : "))
-# num_split(n)
+n = int(input("enter the number : "))
+num_split(n)
 
 
-# problem number 04 - count digit in a number 
+problem number 04 - count digit in a number 
 
-# x1 = int(input("enter the first number : "))
+x1 = int(input("enter the first number : "))
 
-# print(len(str(x1)))
+print(len(str(x1)))
 
 # problem number 05 - sum of digits in a number
 
-# def sum_digit(n):
+def sum_digit(n):
 
-#     sum = 0
-#     for i in str(n):
-#         sum += int(i)
+    sum = 0
+    for i in str(n):
+        sum += int(i)
         
-#     print("sum of digits is : ", sum)
+    print("sum of digits is : ", sum)
     
-# n = int(input("enter the number : "))
-# sum_digit(n)
+n = int(input("enter the number : "))
+sum_digit(n)
 
 
 # problem number 06 - divisible by 3 and 5
-# def div_num(n):
+
+def div_num(n):
     
-#     divisibleby_35 = []
-#     for i in range(n):
-#         if i % 3 == 0 and i % 5 == 0:
-#             divisibleby_35.append(i)
+    divisibleby_35 = []
+    for i in range(n):
+        if i % 3 == 0 and i % 5 == 0:
+            divisibleby_35.append(i)
     
-#     print(divisibleby_35)     
+    print(divisibleby_35)     
     
-# n = int(input("enter the number : "))
-# div_num(n)
+n = int(input("enter the number : "))
+div_num(n)
 
 # problem number 07 - simple calculator making function
 
-# def calculator_sim(a, b , op):
+def calculator_sim(a, b , op):
     
-#     if op == '+':
-#         return a + b
-#     elif op == '-':
-#         return a-b
-#     elif op == '*':
-#         return a * b
-#     elif op == '/':
-#         return a / b
-#     else:
-#         return "invalid operator enter correct operator.."
+    if op == '+':
+        return a + b
+    elif op == '-':
+        return a-b
+    elif op == '*':
+        return a * b
+    elif op == '/':
+        return a / b
+    else:
+        return "invalid operator enter correct operator.."
     
-# a = int(input("enter the first number : "))
-# b = int(input("enter the second number : "))
-# op = input("enter the operator :")
+a = int(input("enter the first number : "))
+b = int(input("enter the second number : "))
+op = input("enter the operator :")
     
-# print(calculator_sim(a, b, op))
+print(calculator_sim(a, b, op))
 
 
 # problem number 08 - prime number check function
 
-# n = int(input("Number enter karo: "))
-# count = 0
+n = int(input("Number enter karo: "))
+count = 0
 
-# for i in range(1, n+1):
-#     if n % i == 0:
-#         count += 1
+for i in range(1, n+1):
+    if n % i == 0:
+        count += 1
 
-# if count == 2:
-#     print("Prime number")
-# else:
-#     print("Not a prime number")
+if count == 2:
+    print("Prime number")
+else:
+    print("Not a prime number")
 
 # problem number 10 - number guessing system 
 
